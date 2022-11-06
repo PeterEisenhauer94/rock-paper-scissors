ad1287483d76efdc85509f5287d29ba890d6daa0
Peter Eisenhauer <petereisenhauer94@gmail.com>
2022-11-06 14:33:36 -0500

    create rps logic

--- javascript.js
+++ javascript.js
@@ -10,6 +10,36 @@ function getComputerChoice(){
     }
 }
 
-function rockPaperScissors(playerSelection,computerSelection){
+function rpsLogic(playerSelection,computerSelection){
+    switch(playerSelection){
+        case 'rock':
+            if(computerSelection==='rock'){
+                return 'Tie game! Try again...';
+            } else if (computerSelection==='paper'){
+                return 'Paper beats Rock! You lose...';
+            } else {
+                return 'Rock beats Scissors! You win!!!';
+            }
+        case 'paper':
+            if(computerSelection==='rock'){
+                return 'Paper beats Rock! You win!!!';
+            } else if (computerSelection==='paper'){
+                return 'Tie game! Try again...';
+            } else {
+                return 'Scissors beats Paper! You lose...';
+            }
+        case 'scissors':
+            if(computerSelection==='rock'){
+                return 'Rock beats Scissors! You lose...';
+            } else if (computerSelection==='paper'){
+                return 'Scissors beats Paper! You win!!!';
+            } else {
+                return 'Tie game! Try again...';
+            }
 
+    }
 }
+
+const playerSelection = 'rock';
+const computerSelection = getComputerChoice();
+console.log(rpsLogic(playerSelection,computerSelection));
